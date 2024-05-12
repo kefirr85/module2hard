9d8de3c64db7513ecd7737aa0a119a0acdf8a997
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-05-12 15:42:28 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,12 @@
+def generate_password(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+
+for n in range(3, 21):
+    password = generate_password(n)
+    print(f'{n} - {password}')
